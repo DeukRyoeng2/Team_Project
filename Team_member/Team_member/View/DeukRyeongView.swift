1de51c5afcbd3f9eb88c5f8343f3fc6e6877fbc2
DeukYeong <polcet112@gmail.com>
2024-05-27 17:06:15 +0900

    프로젝트 push

---
+++ Team_member/Team_member/View/DeukRyeongView.swift
@@ -0,0 +1,8 @@
+//
+//  DeukRyeongView.swift
+//  Team_member
+//
+//  Created by DEUKRYEONG LEE on 5/27/24.
+//
+
+import Foundation
---
+++ Team_member/Team_member/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Team_member
+//
+//  Created by DEUKRYEONG LEE on 5/27/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
